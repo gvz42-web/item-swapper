46954f785a727b855927c624f706109cb1e05c62
gvz42-web <gvz42.web@gmail.com>
2024-01-20 02:49:49 +0200

    Create login page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,32 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import {Router, RouterOutlet} from '@angular/router';
+import {ButtonModule} from "primeng/button";
+import {ApiService} from "./api.service";
+import {CardModule} from "primeng/card";
+import {AuthService} from "./auth.service";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, ButtonModule, CardModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.sass'
 })
-export class AppComponent {
+export class AppComponent{
   title = 'item-swapper';
+  text = '';
+
+  constructor(private api: ApiService, private auth: AuthService, private router: Router) {
+    if (this.auth.isLoggedIn()) {
+      this.router.navigate(['panel'])
+    } else {
+      this.router.navigate(['auth'])
+    }
+  }
+
+  login() {
+    this.api.login('gvz', '123456')
+      .subscribe((data) => this.text = JSON.stringify(data))
+  }
 }
